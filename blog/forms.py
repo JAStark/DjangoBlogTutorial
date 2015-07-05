de1587b0756488897e5f32b87e5426cdbf1282f0
Jennifer Stark <jastark1@gmail.com>
2015-07-05 19:28:20 -0400

    Added homework: post delete, unpublished posts, security, blog comments, postreSQL

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):  #  inherit class to make correct type of model
     class Meta:                   #  tell Django which model to yse to create this form (model = Post)
         model = Post
-        fields = ('title', 'text',)  #  only these 2 because author will be whoever is logged in. 
+        fields = ('title', 'text',)  #  only these 2 because author will be whoever is logged in.
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
